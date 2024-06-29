14fb21089c31ad2fc69569cd2bd34d706af07847
Amirreza Vajdi <Amirreza.Vajdy@gmail.com>
2024-06-29 19:54:14 +0330

    refactror : Back to end change , Identity set on

--- 01_Framework/Domain/EntityBase.cs
+++ 01_Framework/Domain/EntityBase.cs
@@ -6,7 +6,6 @@
         public DateTime CreationDate { get; private set; }
         public EntityBase()
         {
-            Id = Random.Shared.NextInt64(111111111, 999999999);
             CreationDate = DateTime.Now;
         }
     }
