a6c51f351cbb6da22eab06b30ff8d23e8ed4ee49
Amirreza Vajdi <Amirreza.Vajdy@gmail.com>
2024-07-01 14:28:50 +0330

    refactor : Delete Seed Data in Account & Role Mapping

--- AccountManagement.Infrastructure.EFCore/Mapping/AccountMapping.cs
+++ AccountManagement.Infrastructure.EFCore/Mapping/AccountMapping.cs
@@ -15,11 +15,6 @@ namespace AccountManagement.Infrastructure.EFCore.Mapping
             builder.Property(x => x.Password).HasMaxLength(1000).IsRequired();
             builder.Property(x => x.Mobile).HasMaxLength(20).IsRequired();
             builder.Property(x => x.ProfilePhoto).HasMaxLength(500).IsRequired(false);
-
-            builder.HasOne(x => x.Role).WithMany(x => x.Accounts).HasForeignKey(x => x.RoleId);
-            var account = new Account("کاربر ادمین پیش فرض", "admin", "10000.zmlstgGPOBLwzIQhde+BpQ==.WuOA2mKKSjZbDXpF23/y29s1c1EBPaNMk2iFeYttUhE=", "09999999999", 1, "");
-
-            //builder.HasData(account);
         }
     }
 }
