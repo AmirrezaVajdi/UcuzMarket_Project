d2a509c9125e4457a8be651f3575df772b238103
Amirreza Vajdi <Amirreza.Vajdy@gmail.com>
2024-07-24 21:28:08 +0330

    feat : Delivery Application Implementation

---
+++ DeliveryManagement.Application/DeliveryApplication.cs
@@ -0,0 +1,45 @@
+﻿using _01_Framework.Application;
+using DeliveryManagement.Application.Contract.Delivery;
+using DeliveryManagement.Domain.DeliveryAgg;
+
+namespace DeliveryManagement.Application
+{
+    public class DeliveryApplication : IDeliveryApplication
+    {
+        private readonly IDeliveryRepository _deliveryRepository;
+
+        public DeliveryApplication(IDeliveryRepository deliveryRepository)
+        {
+            _deliveryRepository = deliveryRepository;
+        }
+
+        public OperationResult Create(CreateDelivery command)
+        {
+            OperationResult operation = new();
+            var role = new Delivery(command.Address, command.PostalCode, command.AccountId);
+
+            _deliveryRepository.Create(role);
+            _deliveryRepository.SaveChanges();
+            return operation.Succeded();
+        }
+
+        public OperationResult Edit(EditDelivery command)
+        {
+            OperationResult operation = new();
+
+            var delivery = _deliveryRepository.Get(command.Id);
+            if (delivery == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            delivery.Edit(command.Address, command.PostalCode);
+
+            _deliveryRepository.SaveChanges();
+            return operation.Succeded();
+        }
+
+        public List<DeliveryViewModel> List()
+        {
+            return _deliveryRepository.List();
+        }
+    }
+}
