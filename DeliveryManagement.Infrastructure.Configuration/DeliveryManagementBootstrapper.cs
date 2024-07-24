0ffb8b3a4c2eddccaaba317e390082963b014ac5
Amirreza Vajdi <Amirreza.Vajdy@gmail.com>
2024-07-24 21:28:19 +0330

    feat : Delivery Configuration Implementation

---
+++ DeliveryManagement.Infrastructure.Configuration/DeliveryManagementBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using DeliveryManagement.Application;
+using DeliveryManagement.Application.Contract.Delivery;
+using DeliveryManagement.Domain.DeliveryAgg;
+using DeliveryManagement.Infrastructure.EfCore;
+using DeliveryManagement.Infrastructure.EfCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeliveryManagement.Infrastructure.Configuration
+{
+    public class DeliveryManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connctionString)
+        {
+            services.AddTransient<IDeliveryRepository, DeliveryRepository>();
+            services.AddTransient<IDeliveryApplication, DeliveryApplication>();
+
+            services.AddDbContext<DeliveryContext>(x => x.UseSqlServer(connctionString));
+        }
+    }
+}
