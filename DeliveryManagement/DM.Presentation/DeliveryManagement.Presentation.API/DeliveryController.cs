59c492131331f3910124ba36c29e1382a6119c4e
Amirreza Vajdi <Amirreza.Vajdy@gmail.com>
2024-07-26 00:58:35 +0330

    feat : delivery api implementation

---
+++ DeliveryManagement/DM.Presentation/DeliveryManagement.Presentation.API/DeliveryController.cs
@@ -0,0 +1,32 @@
+﻿using _01_Framework.Application;
+using DeliveryManagement.Application.Contract.Delivery;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DeliveryManagement.Presentation.API
+{
+    [Route("api/{controller}")]
+    [ApiController]
+    public class DeliveryController : ControllerBase
+    {
+        private readonly IDeliveryApplication _deliveryApplication;
+        private IAuthHelper _helper;
+
+        public DeliveryController(IDeliveryApplication deliveryApplication, IAuthHelper helper)
+        {
+            _deliveryApplication = deliveryApplication;
+            _helper = helper;
+        }
+
+        [HttpPost("SetDefaultAddress")]
+        public string SetDefaultAddress([FromBody] SetToDefaultDelivery command)
+        {
+            if (_helper.IsAuthenticated())
+            {
+                var result = _deliveryApplication.SetToDefaultDelivery(command);
+                return JsonConvert.SerializeObject(result);
+            }
+            return "Please Login to System";
+        }
+    }
+}
