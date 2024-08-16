dce50b6db289a18e225931a2ae79568a25d53580
Amirreza Vajdi <Amirreza.Vajdy@gmail.com>
2024-08-16 22:34:20 +0330

    refactor : Add a field pageNumber to PaginationOptions for save only positive number

--- 01_Framework/Application/Paginations/PaginationOptions.cs
+++ 01_Framework/Application/Paginations/PaginationOptions.cs
@@ -1,4 +1,25 @@
 ﻿namespace _01_Framework.Application.Pagination
 {
-    public record PaginationOptions(int PageSize = 16, int PageNumber = 1);
+    public record PaginationOptions
+    {
+        private int pageNumber;
+        public int PageSize { get; set; }
+        public int PageNumber
+        {
+            get
+            {
+                return pageNumber;
+            }
+            set
+            {
+                pageNumber = Math.Abs(value);
+            }
+        }
+
+        public PaginationOptions(int PageSize = 16, int PageNumber = 1)
+        {
+            this.PageSize = PageSize;
+            this.PageNumber = PageNumber;
+        }
+    }
 }
